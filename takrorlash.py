54461814cb721d45dbfff4fa43b4cbc1b1a239a8
abduqodir2287 <kadyrovk316@gmail.com>
2023-08-19 16:13:27 +0500

    darsda otilgan mavzulani takrorlash

---
+++ takrorlash.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+# def copher(name:str,key:int):
+#     shifr = ""
+#     for i in name:
+#         shifr += chr(ord(i) + key)
+#
+#     return shifr
+#
+# def decopher(name:str,key:int):
+#     shifr = ""
+#     for i in name:
+#         shifr += chr(ord(i) - key)
+#
+#     return shifr
+#
+# print(copher("Umar",10))
+# print(decopher("_wk|",10))
+
+# def copher(name,key):
+#     return list(map(lambda x:chr(ord(x) + key),name))
+#
+# def decopher(name,key):
+#     return list(map(lambda x:chr(ord(x) - key),name))
+#
+#
+# print(copher("Abduqodir",2))
+# print(decopher("Cdfwsqfkt",2))
+
+now = datetime.now()
+# print(now)
+
+# print(now.year)
+# print(now.month)
+# print(now.day)
+# print(now.hour)
+# print(now.minute)
+# print(now.second)
+# print(now.microsecond)
+
+# print(
+#       now.strftime("%H:%M\n"
+#                    "%d.%m.%Y"))
+
+
+
+
+
+
+
+
+
