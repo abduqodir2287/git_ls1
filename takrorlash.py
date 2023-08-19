076db5f35fa3a89483c00a7a8d66227bdd9bdd77
abduqodir2287 <kadyrovk316@gmail.com>
2023-08-19 16:16:14 +0500

    prosto komment qilish esdan chiqipti

--- takrorlash.py
+++ takrorlash.py
@@ -1,10 +1,10 @@
-from datetime import datetime
-
+# from datetime import datetime
+#
 # def copher(name:str,key:int):
 #     shifr = ""
 #     for i in name:
 #         shifr += chr(ord(i) + key)
-#
+# 
 #     return shifr
 #
 # def decopher(name:str,key:int):
@@ -16,7 +16,7 @@ from datetime import datetime
 #
 # print(copher("Umar",10))
 # print(decopher("_wk|",10))
-
+#
 # def copher(name,key):
 #     return list(map(lambda x:chr(ord(x) + key),name))
 #
@@ -26,10 +26,10 @@ from datetime import datetime
 #
 # print(copher("Abduqodir",2))
 # print(decopher("Cdfwsqfkt",2))
-
-now = datetime.now()
+#
+# now = datetime.now()
 # print(now)
-
+#
 # print(now.year)
 # print(now.month)
 # print(now.day)
@@ -37,7 +37,7 @@ now = datetime.now()
 # print(now.minute)
 # print(now.second)
 # print(now.microsecond)
-
+#
 # print(
 #       now.strftime("%H:%M\n"
 #                    "%d.%m.%Y"))
